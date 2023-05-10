c91990fc36fcbcd4264524459dc6817f085ae5f8
MatteoSensi <matteosensi04@gmail.com>
2023-05-10 12:01:19 +0200

    creazione file php

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
-<html>
-    <head>
-        <title>
-        Esercitazione Sistemi
-        </title>
-    </head>
-    <body>
-        ciao come va
-    </body>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercitazione Sistemi</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>ciaooo</h1>
+</body>
 </html>
